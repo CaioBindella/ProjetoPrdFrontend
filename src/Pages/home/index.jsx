e864ce4858cb6f9e152b193851ac12ef04fa239f
João Matheus <joaomatheusramos@gmail.com>
2022-12-21 14:01:31 -0300

    initial app

---
+++ src/Pages/home/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Button } from './style';
+import { Text } from 'react-native';
+
+const Home = () => {
+	return (
+		<Text>Oi</Text>
+		// <Container>
+		// 	{/* <Button>
+		// 		<Text>Cadastrar Aterro</Text>
+		// 	</Button> */}
+		// 	{/* <Button title='Meus Aterro' /> */}
+		// </Container>
+	);
+};
+
+export default Home;
---
+++ src/Pages/home/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native'
+
+
+export const Container = styled.View`
+    background-color: #fff;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+    color: red;
+    /* margin-top: 5%; */
+`;
+
+export const Text = styled.Text`
+
+`;
