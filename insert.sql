43d5da93b21b906a39a93914a6a49c20ee5c196c
Victor Luis Teixeira Reis <victorluis2002@gmail.com>
2023-04-26 17:20:48 -0300

    terminando tabela indicador

---
+++ insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO Indicador
+(CodInd, Titulo, DescInd, CodSubCat) 
+
+VALUES
+(78, "Existência de banco de dados  contendo os registros operacionais do aterro, especificando dia e hora do recebimento, placa, tipo, volume e peso bruto do caminhão, tara e peso líquido (estimado) e a tipologia do resíduo disposto classificado conforme a Lei 12.305/10;", "" , 8)
+
+
+
