d9799fd30ce882fae976c67b560d8202b2d29072
CaioBindella <caionogueirabindella@gmail.com>
2023-07-15 12:44:41 -0300

    Grafico Polar

--- src/Pages/IndicesOptions/Dashboard/Index.jsx
+++ src/Pages/IndicesOptions/Dashboard/Index.jsx
@@ -3,12 +3,13 @@ import React , { useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 // Native Components
-import { Container, Content } from './Style';
+import { Container, Content, Description, DescriptionContent, Title } from './Style';
 
 import Header from '../../Components/Header/Index';
 import { indiceDb } from "../../../Services/SqlTables/sqliteDb";
 
-import BarChart from "../HorizontalBarChart/Index";
+
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryPolarAxis } from "victory-native";
 
 
 const getMaxScores = () => {
@@ -52,7 +53,7 @@ const Dashboard = ({ navigation, route }) => {
 
     const arrayDeObjetos = [
       { nome: "Objeto 1", numero: 10, taxa: 98 },
-      { nome: "Objeto 2", numero: 20, taxa: 93 },
+      { nome: "Objeto 2", numero: 20, taxa: 55 },
       { nome: "Objeto 3", numero: 30, taxa: 60 },
     ];
 
@@ -62,9 +63,52 @@ const Dashboard = ({ navigation, route }) => {
         <Header title={`Dashboard Técnico - ${aterroData.Nome}`}/>
 
         <Content>
-          {arrayDeObjetos.map((objeto, index) => (
+          {/* {arrayDeObjetos.map((objeto, index) => (
             <BarChart key={index} name={objeto.nome} number={objeto.numero} taxa={objeto.taxa}/>
-        ))}
+        ))} */}
+
+        <VictoryChart polar
+            theme={VictoryTheme.material}
+          >
+            {
+              ["1", "2", "3", "4", "5", "6", "7", "8"].map((d, i) => {
+                return (
+                  <VictoryPolarAxis dependentAxis
+                    key={i}
+                    label={d}
+                    labelPlacement="perpendicular"
+                    style={{ tickLabels: { fill: "none" } }}
+                    axisValue={d}
+                  />
+                );
+              })
+            }
+            <VictoryBar
+              style={{ data: { fill: "tomato", width: 25 } }}
+              data={[
+                { x: "1", y: 10 },
+                { x: "2", y: 25 },
+                { x: "3", y: 40 },
+                { x: "4", y: 50 },
+                { x: "5", y: 50 },
+                { x: "6", y: 30 },
+                { x: "7", y: 50 },
+                { x: "8", y: 30 }
+              ]}
+            />
+          </VictoryChart>
+
+          <DescriptionContent>
+              <Title>Número relacionado a Sub-área</Title>
+              <Description>1: Características fisiográficas</Description>
+              <Description>2: Interface socioambiental</Description>
+              <Description>3: Sistema viário público de acesso</Description>
+              <Description>4: Avaliação da infraestrutura implantada</Description>
+              <Description>5: Avaliação do sistema de controle implantado</Description>
+              <Description>6: Caracteristicas operacionais</Description>
+              <Description>7: Avaliação da Eficiência dos Sistemas de Controle</Description>
+              <Description>8: Documentos básicos e diretrizes operacionais</Description>
+          </DescriptionContent>
         </Content>
 
     </Container>
--- src/Pages/IndicesOptions/Dashboard/Style.js
+++ src/Pages/IndicesOptions/Dashboard/Style.js
@@ -4,8 +4,26 @@ export const Container = styled.View`
 `;
 
 export const Content = styled.View`
+    width: 95%;
     border: 1px;
     margin: 10px;
     border-radius: 10px;
     background-color: #d9d9d9;
+    align-items: center;
+`;
+
+export const Description = styled.Text`
+    font-size: 16px;
+`;
+
+export const DescriptionContent =styled.View`
+    margin: 10px;
+    margin-top: 0px;
+`;
+
+export const Title = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    padding: 5px;
 `;
