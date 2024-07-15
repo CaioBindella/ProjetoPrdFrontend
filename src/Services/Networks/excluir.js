d54ac2220d24608dcc28122686b6c5343d1c1baf
Caio <caionogueirabindella@gmail.com>
2024-07-15 14:37:02 -0300

    Resolvendo exclusão de aterros com análises cadastradas

--- src/Services/Networks/excluir.js
+++ src/Services/Networks/excluir.js
@@ -3,25 +3,54 @@ import municipio from '../SqlTables/municipio'
 import organizacao from '../SqlTables/organizacao'
 import porte from '../SqlTables/porte'
 import analise from '../SqlTables/analise';
+import { indiceDb } from '../SqlTables/sqliteDb';
+import { Alert } from 'react-native';
 
 import axios from 'axios';
 import { localStorage, baseUrl } from '../config';
 
 export const excluir = async (id, table) => {
-    switch (table){
+
+    const getAnalysis = () => {
+        return new Promise((resolve, reject) => {
+            indiceDb.then((data) => {
+                data.transaction((tx) => {
+                    // SQL command
+                    tx.executeSql(
+                        `SELECT * FROM Analise WHERE CodAterro = ? ORDER BY CAST(SUBSTR(DataIni, 0, 3) AS UNSIGNED);`,
+                        [id],
+                        (_, { rows }) => resolve(rows._array),
+                        (_, error) => reject(error) // internal error in tx.executeSql
+                    );
+                });
+            });
+        });
+    }
+    
+    switch (table) {
         case 'aterro':
-            if(localStorage){
-                return await aterro.remove(id)
-            }
-            else{
+            if (localStorage) {
+                getAnalysis()
+                    .then((analyses) => {
+                        if (analyses.length > 0) {
+                            Alert.alert("Erro", "Não é possível deletar esse aterro pois existem análises cadastradas!");
+                        }else {
+                            Alert.alert("Sucesso", "Aterro deletado!");
+                            return aterro.remove(id) 
+                        }
+                    })
+                    .catch((error) => {
+                        console.error("Error fetching analyses:", error);
+                    });
+            } else {
                 axios
-            	.post(`${baseUrl}/aterro`, data)
-                	.then((response) => {
-                		console.log(response);
-                	})
-                	.catch((error) => console.log(JSON.stringify(error)));
+                    .post(`${baseUrl}/aterro`, data)
+                    .then((response) => {
+                        console.log(response);
+                    })
+                    .catch((error) => console.log(JSON.stringify(error)));
             }
-            break
+            break;
         case 'municipio':
             if(localStorage){
                 return await municipio.remove(id)
